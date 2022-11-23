5751f849a7fb4888279d85e6c44f3db15ec8f4ae
Felipe Maia Vieira <fmaiavieira@gmail.com>
2022-11-23 14:24:42 -0300

    feat(microapps): created SPA and react microapp

---
+++ shell/react-micro-app/src/root.component.tsx
@@ -0,0 +1,7 @@
+export default function Root(props) {
+  return (
+    <div>
+      <div>React Microapp</div>
+    </div>
+  );
+}
---
+++ shell/src/fmv-root-config.ts
@@ -0,0 +1,24 @@
+import { registerApplication, start } from "single-spa";
+import {
+  constructApplications,
+  constructRoutes,
+  constructLayoutEngine
+} from "single-spa-layout";
+import microfrontendLayout from "./microfrontend-layout.html";
+
+const routes = constructRoutes(microfrontendLayout);
+const applications = constructApplications({
+  routes,
+  loadApp({ name }) {
+    return System.import(name);
+  }
+});
+const layoutEngine = constructLayoutEngine({ routes, applications });
+// registerApplication(
+//   "@fmv/header-react",
+//   () => System.import("@fmv/react-micro-app") as any,
+//   (location) => true
+// );
+applications.forEach(registerApplication);
+layoutEngine.activate();
+start();
